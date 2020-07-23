e876305eb08cab6722e62c44806c06cbf8a2c9f8
PatrycjaDom <patcho2808@gmail.com>
2020-07-23 08:05:22 +0200

    Added functions to handleForm.php

--- app/handleForm.php
+++ app/handleForm.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 use \CloudConvert\CloudConvert;
 use \CloudConvert\Models\Job;
 use \CloudConvert\Models\Task;
@@ -8,30 +10,71 @@ if(!isset($_POST['ebook_file'])|| !isset($_POST['user_email']))
 {
     header('index.php');
 }
-var_dump($_FILES['ebook_file']);
 
-//$cloudconvert = new CloudConvert([
-//    'api_key' => file_get_contents('API_KEY.txt'),
-//    'sandbox' => false
-//]);
-//
-//
-//$job = (new Job())
-//    ->addTask(
-//        (new Task('import/url', 'import_epub'))
-//            ->set('url', '/path/to/file')
-//    )
-//    ->addTask(
-//        (new Task('convert', 'convert_epub_to_mobi'))
-//            ->set('input_format', 'epub')
-//            ->set('output_format', 'mobi')
-//            ->set('engine', 'calibre')
-//            ->set('input', ["import_epub"])
-//    )
-//    ->addTask(
-//        (new Task('export/url', 'export_mobi'))
-//            ->set('input', ["convert_epub_to_mobi"])
-//            ->set('inline', 'false')
-//            ->set('archive_multiple_files', 'false')
-//    );
-//$cloudconvert->jobs()->create($job);
+function convertFile()
+{
+    $cloudconvert = new CloudConvert([
+        'api_key' => file_get_contents('API_KEY.txt'),
+        'sandbox' => false
+    ]);
+
+    $job = getJob();
+    convert($cloudconvert, $job);
+}
+
+/**
+ * @param CloudConvert $cloudconvert
+ * @param Job $job
+ */
+function convert(CloudConvert $cloudconvert, Job $job)
+{
+    try {
+        $cloudconvert->jobs()->create($job);
+        $uploadTask = $job->getTasks()->name('import_epub')[0];
+        move_uploaded_file($_FILES['ebook_file']['tmp_name'], '/tmp/input/' . $_FILES['ebook_file']['name']);
+        $cloudconvert->tasks()->upload($uploadTask, fopen($_FILES['ebook_file']['name'], 'r'));
+        $cloudconvert->jobs()->wait($job); // Wait for job completion
+
+        foreach ($job->getExportUrls() as $file) {
+
+            $source = $cloudconvert->getHttpTransport()->download($file->url)->detach();
+            $dest = fopen('/tmp/output/' . $file->filename, 'w');
+
+            stream_copy_to_stream($source, $dest);
+
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+/**
+ * @return Job
+ */
+function getJob()
+{
+    $job = (new Job())
+        ->addTask(
+            (new Task('import/upload', 'import_epub'))
+        )
+        ->addTask(
+            (new Task('convert', 'convert_epub_to_mobi'))
+                ->set('input_format', 'epub')
+                ->set('output_format', 'mobi')
+                ->set('engine', 'calibre')
+                ->set('input', ["import_epub"])
+        )
+        ->addTask(
+            (new Task('export/url', 'export_mobi'))
+                ->set('input', ["convert_epub_to_mobi"])
+        );
+    return $job;
+}
+
+function sendFileToKindle()
+{
+
+}
+
+convertFile();
+sendFileToKindle();
--- app/index.php
+++ app/index.php
@@ -1,4 +1,4 @@
 <?php
 
-   require('view.html');
-    //phpinfo();
+require('view.html');
+    phpinfo();
